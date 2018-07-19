1d6a68035c9c55d846bab653f4262bafc5710ecd
siddh4489 <siddh4489@gmail.com>
2018-07-19 15:21:58 +0530

    Update SalesforcePodioRestApi.java

--- src/main/java/com/sfdc/podio/rest/SalesforcePodioRestApi.java
+++ src/main/java/com/sfdc/podio/rest/SalesforcePodioRestApi.java
@@ -21,30 +21,46 @@ import javax.ws.rs.core.Response;
 @Path("/podio")
 public class SalesforcePodioRestApi {
 
+    @GET
+    @Path("/{param}")
+    public Response getMsg(@PathParam("param") String msg) {
+        String output = "Jersey say : " + msg;
+        return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Path("/totalProperty")
+    public Response getTotalItems() {
+        String data = "{\"title\":\"Sergey\",\"name\":\"Kargopolov\"}";
+        //Integer ItemId = SalesforcePodioOperation.newProperty(SalesforcePodioDataParser.jsonToObject(data));
+        String output = "Total Items : " + SalesforcePodioOperation.getTotalItems();
+        return Response.status(200).entity(output).build();
+    }
+
     @POST
     @Path("/newProperty")
     public Response newProperty(String data, @HeaderParam("itemId") Integer itemId) {
         String itemref = "";
-        System.out.println(" data : "+data);
-        System.out.println(" itemId : "+itemId);
+        System.out.println(" data : " + data);
+        System.out.println(" itemId : " + itemId);
         System.out.println(" called method : newProperty ");
         itemId = (itemId != null ? itemId : 0);
         if (SalesforcePodioOperation.existingPropertyCheck(itemId)) {
             System.out.println(" In  : Update Property ");
             boolean checkUpdate = SalesforcePodioOperation.updateProperty(itemId, SalesforcePodioDataParser.jsonToObject(data));
-            System.out.println(" Update Property Status :"+checkUpdate);
-            if(checkUpdate){
+            System.out.println(" Update Property Status :" + checkUpdate);
+            if (checkUpdate) {
                 itemref = "Updated Successfully";
-            }else{
+            } else {
                 itemref = "Update Failed";
             }
         } else {
             System.out.println(" In : New Property");
             itemref = SalesforcePodioOperation.newProperty(SalesforcePodioOperation.getApplicationId("Test App"), SalesforcePodioDataParser.jsonToObject(data)).toString();
-            System.out.println("Created Property itemid : "+itemref);
-            itemref = "Created Property ItemId '"+itemref+"'";
+            System.out.println("Created Property itemid : " + itemref);
+            itemref = "Created Property ItemId '" + itemref + "'";
         }
-        return Response.status(201).entity("Status : "+itemref.toString()).build();
+        return Response.status(201).entity("Status : " + itemref.toString()).build();
     }
-    
+
 }
